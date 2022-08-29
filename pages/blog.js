44bb2a16a6cb2b1adc42de35545088bace1a55ac
Kai Takami <kaitakami@protonmail.com>
2022-08-29 22:38:21 +0900

    Add blog page with dinamic routing using Next.js

---
+++ pages/blog.js
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import Layout from "../components/Layout";
+import Entrada from "../components/Entrada";
+import styles from "../styles/Blog.module.css";
+
+const Blog = ({ entradas }) => {
+  return (
+    <Layout pagina="Blog">
+      <main className="contenedor">
+        <h2 className="heading">Blog</h2>
+        <div className={styles.blog}>
+          {entradas.map((entrada) => (
+            <Entrada key={entrada.id} entrada={entrada} />
+          ))}
+        </div>
+      </main>
+    </Layout>
+  );
+};
+
+export async function getStaticProps() {
+  const url = `${process.env.API_URL}/blogs?_sort=created_at:desc`;
+  const response = await fetch(url);
+  const entradas = await response.json();
+  return {
+    props: {
+      entradas,
+    },
+  };
+}
+
+export default Blog;
---
+++ pages/blog/[url].js
@@ -0,0 +1,54 @@
+import Image from 'next/image'
+import Layout from "../../components/Layout";
+import { formatearFecha } from '../../helpers';
+
+import styles from '../../styles/Entrada.module.css'
+
+const EntradaBlog = ({ entrada }) => {
+  const { contenido, imagen, published_at, titulo } = entrada[0];
+
+  return (
+    <Layout
+      pagina={titulo}
+    >
+      <main className="contenedor">
+        <h1 className="heading">{titulo}</h1>
+        <article className={styles.entrada}>
+            <Image layout='responsive' width={800} height={600} src={imagen.url} alt={`Imagen entrada ${titulo}`} />
+            <div className={styles.contenido}>
+                <p className={styles.fecha}>{formatearFecha(published_at)}</p>
+                <p className={styles.texto}>{contenido}</p>
+            </div>
+        </article>
+      </main>
+    </Layout>
+  );
+};
+
+export async function getStaticPaths() {
+  const url = `${process.env.API_URL}/blogs`;
+  const response = await fetch(url);
+  const entradas = await response.json();
+
+  const paths = entradas.map((entrada) => ({
+    params: { url: entrada.url },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params: { url } }) {
+  const urlBlog = `${process.env.API_URL}/blogs?url=${url}`;
+  const response = await fetch(urlBlog);
+  const entrada = await response.json();
+  return {
+    props: {
+      entrada,
+    },
+  };
+}
+
+export default EntradaBlog;
